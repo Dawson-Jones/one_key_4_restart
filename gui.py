e1ba49b3273d215334ef5185dbe3c3005b3fbca1
PhoeniXWhite <957360688@qq.com>
2020-01-05 22:39:33 +0800

    finish?

--- gui.py
+++ gui.py
@@ -3,6 +3,7 @@ from resource.restart_ai_on_opt import Ui_Form
 
 
 class OneKey(QWidget, Ui_Form):
+    send_info_signal = pyqtSignal(int)
     step_num = 1
 
     def __init__(self):
@@ -11,12 +12,26 @@ class OneKey(QWidget, Ui_Form):
         self.setupUi(self)
 
     def next_step(self):
+        if self.step_num == 4:
+            QCoreApplication.quit()
+            sys.exit()
+        if self.step_num != 2:
+            self.show_img(self.step_num)
+            return
+        reply = QMessageBox.question(self, "", "EL 外是否有组件?", QMessageBox.Yes | QMessageBox.No,
+                                     QMessageBox.Yes)
+        if reply == QMessageBox.Yes:
+            self.send_info_signal.emit(1)
+        elif reply == QMessageBox.No:
+            self.send_info_signal.emit(0)
         self.show_img(self.step_num)
 
     def show_img(self, num):
         self.load_img = QPixmap(f'./resource/img/{num}.jpg')
         self.img_file = self.load_img.scaled(self.img_lb.size(), aspectRatioMode=Qt.KeepAspectRatio)
         self.img_lb.setPixmap(self.img_file)
+        if self.step_num == 3:
+            self.confim.setText("OK")
         self.step_num += 1
 
 
--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import sys
+import requests
 from PyQt5.Qt import *
 from restart_process import one_key
 from gui import OneKey
 
 
-def main():
-    one_key()
-    app = QApplication(sys.argv)
-    window = OneKey()
-    window.show()
-    sys.exit(app.exec_())
+def send_http(flag):
+    if flag:
+        # requests.post()
+        print(flag, type(flag))
+    else:
+        # requests.post()
+        print(flag, type(flag))
 
 
 if __name__ == '__main__':
-    main()
+    # one_key()
+    app = QApplication(sys.argv)
+    window = OneKey()
+    window.show()
+    window.send_info_signal.connect(send_http)
+    sys.exit(app.exec_())
