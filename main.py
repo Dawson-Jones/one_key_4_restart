fb3e7421241be8797b81ac65dc3e0ab9568096f8
PhoeniXWhite <957360688@qq.com>
2020-01-05 13:27:57 +0800

    add ui

--- main.py
+++ main.py
@@ -2,31 +2,49 @@ import time
 import os
 import shutil
 import yaml
+import psutil
 
 
 def main():
     with open('config.yml') as f:
-        config_dict = yaml.load(f)
-    # print(config_dict)
+        config_dict = yaml.load(f, Loader=yaml.Loader)
+    print(config_dict)
+    """
+    {
+        'app_path': ['C:/Users/yc-pc/Desktop/dobby/yc_pc/PLCTest_Kevin/PLCTest_Kevin/PLCTest/bin/Debug/PLCTest.exe',
+                     'C:/Users/yc-pc/Desktop/dobby/yc_pc/el_panel-jkf5-0.4.0/main.exe',
+                     'C:/Users/yc-pc/Desktop/dobby/yc_pc/gui_jkus-0.1.4/gui_jkus-0.1.4/el-jk-us.exe'],
+        'clear_cache_path': ['C:/Users/yc-pc/Desktop/dobby/gui_jkus-0.1.4/gui_jkus-0.1.4/resources/app/src/cache',
+                             'C:/Users/yc-pc/Desktop/dobby/el_panel-jkf5-0.4.0/temp_pic']
+    }
+    """
 
-    deal_process = config_dict.get('app_path')
+    # 参数完整性
+    deal_process_path = config_dict.get('app_path')
     cache_path: list = config_dict.get('clear_cache_path')
-
-    if not all([deal_process, cache_path]):
+    if not all([deal_process_path, cache_path]):
         return
 
-    for i in deal_process:
-        try:
-            os.popen('taskkill /im {} -f'.format(os.path.split(i)[-1]))
-        except Exception as e:
-            print(e)
+    # kill process
+    deal_process_name = [os.path.split(i)[-1] for i in deal_process_path]
+    deal_process_num = len(deal_process_name)
+    pids = psutil.pids()
+    for pid in pids:
+        p = psutil.Process(pid)
+        if p.name() in deal_process_name:
+            os.popen('taskkill /im {} -f'.format(p.name()))
+            deal_process_num -= 1
+        if not deal_process_num:
+            break
 
+    # clear cache
     for i in cache_path:
         if os.path.exists(i):
             shutil.rmtree(i)
             os.makedirs(i)
 
-    for i in deal_process:
+    # restart process
+    for i in deal_process_path:
         os.startfile(i)
 
 
---
+++ resource/restart_ai_on_opt.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'restart_ai_on_opt.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.0
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(645, 450)
+        self.verticalLayout = QtWidgets.QVBoxLayout(Form)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.label = QtWidgets.QLabel(Form)
+        self.label.setStyleSheet("font: 18pt \"Ubuntu Mono\";")
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setObjectName("label")
+        self.verticalLayout.addWidget(self.label)
+        self.label_2 = QtWidgets.QLabel(Form)
+        self.label_2.setStyleSheet("font: 18pt \"Ubuntu Mono\";")
+        self.label_2.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_2.setObjectName("label_2")
+        self.verticalLayout.addWidget(self.label_2)
+        self.pushButton = QtWidgets.QPushButton(Form)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButton.sizePolicy().hasHeightForWidth())
+        self.pushButton.setSizePolicy(sizePolicy)
+        self.pushButton.setObjectName("pushButton")
+        self.verticalLayout.addWidget(self.pushButton)
+        self.verticalLayout.setStretch(0, 4)
+        self.verticalLayout.setStretch(1, 4)
+        self.verticalLayout.setStretch(2, 1)
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.label.setText(_translate("Form", "启动opt电脑上的AI重置程序"))
+        self.label_2.setText(_translate("Form", "Start the AI reset program on the opt computer"))
+        self.pushButton.setText(_translate("Form", "OK"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
+    sys.exit(app.exec_())
